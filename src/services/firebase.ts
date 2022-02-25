032e829fc1d7f3f6b89d8a86429b09f71f62b255
elton <elton.souza1515@gmail.com>
2022-02-25 11:45:19 -0300

    update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,16 @@
+//Conexão com o firebase
+
 import firebase from "firebase/compat/app";
-import 'firebase/compat/auth';
-import 'firebase/compat/database';
-import 'firebase/auth'
+import "firebase/compat/auth";
+import "firebase/compat/database";
 const firebaseConfig = {
-    apiKey: process.env.REACT_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,4 +18,4 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
 
-export {auth, firebase, database}
+export { auth, firebase, database };
